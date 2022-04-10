7ae5b6ccd4c5d157504e52c5850ab1bfbdbafcf2
Guillermo Tinoco <m3mo89@gmail.com>
2022-04-09 21:35:22 -0500

    Add unit test for movie controller

---
+++ WizelineGoBootcampChallengeTests/MovieControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WizelineGoBootcampChallenge.Controllers;
+using WizelineGoBootcampChallenge.Models;
+using WizelineGoBootcampChallenge.Services.Movies;
+using WizelineGoBootcampChallengeTests.Mocks;
+using Xunit;
+
+namespace WizelineGoBootcampChallengeTests
+{
+    public class MovieControllerTest
+    {
+        private readonly MovieController _controller;
+        private readonly IMoviesService _service;
+
+        public MovieControllerTest()
+        {
+            _service = new MoviesServiceFake();
+            _controller = new MovieController(_service);
+        }
+
+        [Fact]
+        public async void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var result = await _controller.Get();
+
+            // Assert
+            var items = Assert.IsType<List<Movie>>(result);
+            Assert.Equal(2, items.Count);
+        }
+    }
+}
